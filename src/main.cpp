a9555850d6618a96007b3dcfda11863930b6c7aa
Long Yang Paffrath <paffrath@yangnet.de>
2024-05-17 00:36:39 +0200

    Disable window resizing

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
