9c681200cdb6127cb6b2ce339d0646f75f835b4e
pyromelonz <pyromelonz@gmail.com>
2024-06-14 14:05:17 +0200

    dumbass little mistakes

--- src/components/mesh.cpp
+++ src/components/mesh.cpp
@@ -19,7 +19,7 @@ void Mesh::Update()
             ->data.model = modelMatrix;
     }
     glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
     auto peen = transform->getPos();
     // std::cout << "object drawn at " << peen.x << ' ' << peen.y << ' ' << peen.z << std::endl;
@@ -27,6 +27,7 @@ void Mesh::Update()
 
 void Mesh::Init()
 {
+    
     LoadMesh();
     LoadTexture();
     glGenVertexArrays(1, &vao);
@@ -36,10 +37,10 @@ void Mesh::Init()
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0])*vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,  indices.size() * sizeof(indices[0]), indices.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
     glEnableVertexAttribArray(0);
